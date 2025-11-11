39e673ef8c20dbb8b9405c2ecaed5f64fbf715a7
zhangzhiwei0905 <1034528234@qq.com>
2025-11-11 21:09:11 +0800

    添加next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.amazingzz.com', // ← 改成你的域名（不能用 localhost）
+  generateRobotsTxt: true,            // 自动生成 robots.txt
+  sitemapSize: 7000,
+};
